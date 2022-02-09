6d65c9d856d0cd3e493430f1fd217fa8fe0b570f
Md-Danish122 <99079123+Md-Danish122@users.noreply.github.com>
2022-02-09 13:20:18 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3002
 
 app.get('/', (req, res) => {
-  res.send('Hello World kya haal h!')
+  res.send('Hello World kya haal h index2')
 })
 
 app.listen(port, () => {
